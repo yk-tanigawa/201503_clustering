f78d76a56150245524e178ac74fafb10daaa9d67
yk-tanigawa <yk-tanigawa@users.noreply.github.com>
2015-02-12 00:21:27 +0900

    debug on Hamerly, it is still too slow

--- program/Hamerly.cpp
+++ program/Hamerly.cpp
@@ -3,10 +3,11 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <cfloat>
 
 #include "my_vector.hpp"
 #include "time_bench.hpp"
@@ -17,6 +18,70 @@ template <class T>
 inline double dist(vector<T> x, vector<T> y){
   return norm(x - y); /* ベクトルのノルムから距離を定義 */
 }
+
+template <class T>
+inline int argmin(vector<T> v){
+  if(v.size() < 2){
+    return 0;
+  }else{
+    int argmin = 0; T minval = v.at(argmin);     
+    for(int i = 1; i < v.size(); ++i){
+      if(v.at(i) < minval){
+	argmin = i; minval = v.at(i);
+      }
+    }
+    return argmin;
+  }
+}
+
+template <class T>
+inline int min(vector<T> v){
+  if(v.size() < 2){
+    return v.at(0);
+  }else{
+    T minval = v.at(argmin);     
+    for(int i = 1; i < v.size(); ++i){
+      if(v.at(i) < minval){
+	minval = v.at(i);
+      }
+    }
+    return minval;
+  }
+}
+
+void Hamerly_update_ul(const int n, const int k,
+		       const vector<vector <double> > data,
+		       const vector<vector<double> > c, /* クラスタの重心 */
+		       const vector<int> a,    /* 各点x_iが属するクラスタ */
+		       vector<double> &u,   /* upper bound */
+		       vector<double> &l    /* lower bound */);
+bool Hamerly_repeat(const int n, const int d, const int k,
+		    const vector<vector <double> > data,
+		    vector<int> &q,      /* クラスタ中の点の数 */
+		    vector<vector<double> > &c,     /* クラスタの重心 */
+		    vector<vector<double> > &c_sum, /* クラスタのベクトル和*/
+		    vector<double> &s,   /* 最近接クラスタの重心との距離 */
+		    vector<int> &a,      /* 各点x_iが属するクラスタ */
+		    vector<double> &u,   /* upper bound */
+		    vector<double> &l    /* lower bound */);
+void Hamerly_init(const int n, const int d, const int k,
+		  const vector<vector <double> > data,
+		  vector<int> &q,      /* クラスタ中の点の数 */
+		  vector<vector<double> > &c,     /* クラスタの重心 */
+		  vector<vector<double> > &c_sum, /* クラスタのベクトル和*/
+		  vector<int> &a,      /* 各点x_iが属するクラスタ */
+		  vector<double> &u,   /* upper bound */
+		  vector<double> &l    /* lower bound */);
+double Hamerly_sqerr(const int n, const int k,
+		     const vector<vector<double> > data,
+		     vector<vector<double> > &c,  /* クラスタの重心 */
+		     vector<int> &a      /* 各点x_iが属するクラスタ */);
+void Hamerly(const int n, const int d, const int k,
+	     const vector<vector <double> > data);
+
+
+/* 下記プログラム本体 */
+
 
 void Hamerly_update_ul(const int n, const int k,
 		       const vector<vector <double> > data,
@@ -49,10 +114,21 @@ bool Hamerly_repeat(const int n, const int d, const int k,
   /* Hamerlyの繰り返しステップ クラスタ重心の更新の有無をbooleanで返す */
   bool updated = false;
 
+#if 0
+  cerr << "***** rep *****" << endl;
+  cerr << "j" << "\t" << "q.at(j)" << "\t" << "c(j)" << endl;
+  for(int j = 0; j < k; ++j){
+    cerr << j << "\t" << q.at(j) << "\t" << c.at(j);
+  }
+
+  cerr << "s(j)" << endl;
+#endif
+  
+
   /* まずs(j)を更新 
    * s[j] = min_{jj != j} dist(c[jj], c[j]) */
   for(int j = 0; j < k; ++j){
-    double min = dist(c.at(j), c.at((j == 0) ? 1 : 0));
+    double min = DBL_MAX;
     for(int jj = 0; jj < k; ++jj){
       if(jj != j){
 	double distance = dist(c.at(j), c.at(jj));
@@ -60,21 +136,23 @@ bool Hamerly_repeat(const int n, const int d, const int k,
       }
     }
     s.at(j) = min;
+#if 0
+    cerr << j << "\t" << s.at(j) << endl;
+#endif
   }
 
   for(int i = 0; i < n; ++i){
-    double m = max(s.at(a.at(i)) / 2.0, l.at(i));
+    double m = max((s.at(a.at(i)) / 2.0), l.at(i));
     if(u.at(i) > m){     /* Hamerlyの命題の条件を満たさない */
       u.at(i) = dist(data.at(i), c.at(a.at(i))); /* upper boundを更新 */
       if(u.at(i) > m){   /* Hamerlyの命題の条件を満たさない */
 	int aa = a.at(i); /* 最近接クラスタが変化したか調べる */
 	{ /* 最近接クラスタを再計算 */
-	  int argmin_j = 0;
-	  double min = dist(data.at(i), c.at(0));
-	  for(int j = 1; j < k; ++j){
-	    if(dist(data.at(i), c.at(j)) < min){ argmin_j = j; }
+	  vector<double> distance(k);
+	  for(int j = 0; j < k; ++j){
+	    distance.at(j) = dist(data.at(i), c.at(j));
 	  }
-	  a.at(i) = argmin_j;
+	  a.at(i) = argmin(distance);
 	}
 	if(aa != a.at(i)){ /* 最近接クラスタの変化によるパラメタの更新*/
 	  updated = true; q.at(aa)--; c_sum.at(aa) -= data.at(i);	 
@@ -116,7 +194,8 @@ void Hamerly_init(const int n, const int d, const int k,
   /* 各クラスタの代表点をランダムに選択 */
   for(int j = 0; j < k; ++j){
     vector<double> zero(d, 0);
-    q.at(j) = 0;  c.at(j) = zero; c_sum.at(j) = zero;
+    q.at(j) = 0;  c.at(j) = data.at(j); c_sum.at(j) = zero;
+    /* j番目のクラスタ中心をj番目のデータと一致させて初期化した */
   }
   for(int i = 0; i < n; ++i){
     int argmin_j = 0;
@@ -134,8 +213,18 @@ void Hamerly_init(const int n, const int d, const int k,
     q.at(argmin_j)++;
     c_sum.at(argmin_j) += data.at(i);
   }
+  for(int j = 0; j < k; ++j){
+    c.at(j) = (1.0 / q.at(j)) * c_sum.at(j);
+  }
   /* upper bound, lower bound の更新 */
   Hamerly_update_ul(n, k, data, c, a, u, l);
+#if 0
+  cerr << "***** init *****" << endl;
+  cerr << "j" << "\t" << "q.at(j)" << "\t" << "c(j)" << endl;
+  for(int j = 0; j < k; ++j){
+    cerr << j << "\t" << q.at(j) << "\t" << c.at(j);
+  }
+#endif
   return;
 }
 
